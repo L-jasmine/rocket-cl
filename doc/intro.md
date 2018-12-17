b63bfd664e0ecd7fa6015961f65320019ad49b2f
tcdk <458761603@qq.com>
2018-12-17 14:17:09 +0800

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rocket-cl
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rocket_cl/core_test.clj
@@ -0,0 +1,7 @@
+(ns rocket-cl.core-test
+  (:require [clojure.test :refer :all]
+            [rocket-cl.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
